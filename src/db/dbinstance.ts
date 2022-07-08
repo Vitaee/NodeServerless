a9f23a4c89bf2bd9d3371a7cc9ccd1a56bf17da0
Vitaee <canilguu@gmail.com>
2022-07-08 23:02:51 +0300

    sls offline updated

--- src/db/dbinstance.ts
+++ src/db/dbinstance.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-
+import * as mysql2 from 'mysql2';
 const db_config  = {
     db: process.env.DB_NAME,
     username: process.env.DB_USER,
@@ -9,6 +9,7 @@ const db_config  = {
 export const sequelize = new Sequelize(db_config.db, db_config.username, db_config.password, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
+    dialectModule: mysql2,
     port: parseInt(process.env.DB_PORT)
 });
 
