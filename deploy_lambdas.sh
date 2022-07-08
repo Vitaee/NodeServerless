7dbc279ea0ebdc049a686f976f59df42b3455b65
Vitaee <canilguu@gmail.com>
2022-07-09 01:53:16 +0300

    updated yml file3

--- deploy_lambdas.sh
+++ deploy_lambdas.sh
@@ -4,14 +4,6 @@ env="production"
 
 echo "Deployment started for environment: " $env
 
-#Creating necessary variables
-fileName="env.json"
-sourceLocation="s3://awsbucketvitae/${fileName}"
-
-# Downloading the config file from S3
-echo "downloading file from s3"
-aws s3 cp $sourceLocation $fileName
-
 #Deploying the lambdas via serverless
 echo "Deploying to Lambda"
 serverless plugin install --name serverless-esbuild serverless-dotenv-plugin serverless-offline 
