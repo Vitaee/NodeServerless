c4f75656940cb41ba03321a2efeceea8290bc05c
Vitaee <canilguu@gmail.com>
2022-08-21 13:46:05 +0300

    project get & update

--- src/api/routes/project.ts
+++ src/api/routes/project.ts
@@ -1,16 +1,35 @@
 import { Response, Router , Request} from "express";
-import { Users } from '../../db/dbinstance';
+import { Projects, Users } from '../../db/dbinstance';
 import { auth, CustomRequest } from "../../middlewares/checkJwt";
 import { createProject } from "../controllers/projects/projectCreate";
 
 const projectsRouter: Router = Router();
 
 projectsRouter.get('/:id', auth, async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200)
+    const { project_id } = req.params;
+    const data = (req as CustomRequest).token;
+
+    const user = await Users.findOne( { where: {email: data['email']} } );
+
+    const toProject = await Projects.findOne( {where: {ownerId: user.getDataValue('id')}} );
+    
+    return toProject ? res.status(200).send( { data: toProject  } ) : res.status(404).send( { data: 'Project does not exists'  } ); 
 });
 
-projectsRouter.put('/update/:id', async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200);
+projectsRouter.put('/update/:id', auth,  async (req: Request, res: Response): Promise<Response> => {
+    const { name } = req.body
+
+    const data = (req as CustomRequest).token;
+
+    const user = await Users.findOne( { where: {email: data['email']} } );
+
+    const updateProject = await Projects.update({ name: name }, {
+        where: {
+          ownerId: user.getDataValue('id')
+        }
+      });
+
+    return updateProject ? res.status(200).send({data:'Project name updated'}) : res.status(500).send({data: 'Error during updating project name'})
 });
 
 projectsRouter.delete('/:id', async (req: Request, res: Response): Promise<Response> => {
